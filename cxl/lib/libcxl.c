396730980b0e2158189cd5888b27d1b0fb9e300f
Praveen Kumar B <praveenkb@meta.com>
2023-11-26 20:05:23 -0800

    Print ltssm state value as string for cxl link status vendor command
    Summary:
    Print ltssm state value as string for cxl link status vendor command
    
    Test Plan:
    Link is in CXL1.1 mode
    Negotiated link width: x16
    Negotiated link speed: Gen5
    ltssm state: L0, code 0x11
    
    Reviewers:
    
    Subscribers:
    
    Tasks: T163788541
    
    Tags:

--- cxl/lib/libcxl.c
+++ cxl/lib/libcxl.c
@@ -10647,6 +10647,46 @@ out:
 #define CXL_MEM_COMMAND_ID_GET_CXL_LINK_STATUS CXL_MEM_COMMAND_ID_RAW
 #define CXL_MEM_COMMAND_ID_GET_CXL_LINK_STATUS_OPCODE 0xFB07
 
+char ltssm_state_name[][20] =
+{
+	"DETECT_QUIET",
+	"DETECT_ACT",
+	"POLL_ACTIVE",
+	"POLL_COMPLIANCE",
+	"POLL_CONFIG",
+	"PRE_DETECT_QUIET",
+	"DETECT_WAIT",
+	"CFG_LINKWD_START",
+	"CFG_LINKWD_ACEPT",
+	"CFG_LANENUM_WAIT",
+	"CFG_LANENUM_ACEPT",
+	"CFG_COMPLETE",
+	"CFG_IDLE",
+	"RCVRY_LOCK",
+	"RCVRY_SPEED",
+	"RCVRY_RCVRCFG",
+	"RCVRY_IDLE",
+	"L0",
+	"L0S",
+	"L123_SEND_EIDLE",
+	"L1_IDLE",
+	"L2_IDLE",
+	"L2_WAKE",
+	"DISABLED_ENTRY",
+	"DISABLED_IDLE",
+	"DISABLED",
+	"LPBK_ENTRY",
+	"LPBK_ACTIVE",
+	"LPBK_EXIT",
+	"LPBK_EXIT_TIMEOUT",
+	"HOT_RESET_ENTRY",
+	"HOT_RESET",
+	"RCVRY_EQ0",
+	"RCVRY_EQ1",
+	"RCVRY_EQ2",
+	"RCVRY_EQ3"
+};
+
 struct cxl_get_cxl_link_status_out {
 	float cxl_link_status;
 	uint32_t link_width;
@@ -10705,7 +10745,7 @@ CXL_EXPORT int cxl_memdev_get_cxl_link_status(struct cxl_memdev *memdev)
 	fprintf(stdout, "Link is in CXL%0.1f mode\n", get_cxl_link_status_out->cxl_link_status);
 	fprintf(stdout, "Negotiated link width: x%d\n", get_cxl_link_status_out->link_width);
 	fprintf(stdout, "Negotiated link speed: Gen%d\n", get_cxl_link_status_out->link_speed);
-	fprintf(stdout, "ltssm state 0x%x\n", get_cxl_link_status_out->ltssm_val);
+	fprintf(stdout, "ltssm state: %s, code 0x%x\n", ltssm_state_name[get_cxl_link_status_out->ltssm_val], get_cxl_link_status_out->ltssm_val);
 out:
 	cxl_cmd_unref(cmd);
 	return rc;
