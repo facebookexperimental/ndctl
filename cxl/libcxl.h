e372b8ded9c9a282700b7a71eebf973bac7078ed
gadhiadevarsh <gadhiadevarsh@snc-fbuser3584.thefacebook.com>
2024-09-27 04:46:21 -0700

    Counting CXL errors for Correctable, Uncorrectable, and CFG err type
    Summary: Counting CXL errors for Correctable, Uncorrectable, and CFG err type
    
    Test Plan: test by cxl-cli and shell cmd prompt
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- cxl/libcxl.h
+++ cxl/libcxl.h
@@ -290,6 +290,7 @@ int cxl_memdev_ddr_hppr_addr_info_clear(struct cxl_memdev *memdev, u8 ddr_id, u8
 int cxl_memdev_ddr_ppr_status_get(struct cxl_memdev *memdev);
 int cxl_memdev_ddr_refresh_mode_set(struct cxl_memdev *memdev, u8 refresh_select_option);
 int cxl_memdev_ddr_refresh_mode_get(struct cxl_memdev *memdev);
+int cxl_memdev_cxl_err_cnt_get(struct cxl_memdev *memdev);
 
 #define cxl_memdev_foreach(ctx, memdev) \
         for (memdev = cxl_memdev_get_first(ctx); \
